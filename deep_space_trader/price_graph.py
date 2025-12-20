41340f099f6049ce76aef64147047e5081ab0646
Erik Nyquist <eknyquist@gmail.com>
2025-12-20 13:43:40 -0800

    Improve look/readability of price history graph

--- deep_space_trader/price_graph.py
+++ deep_space_trader/price_graph.py
@@ -7,6 +7,9 @@ class PriceHistoryGraph(QtWidgets.QDialog):
 
         super(PriceHistoryGraph, self).__init__(parent)
 
+        self.setMinimumWidth(600)
+        self.setMinimumHeight(600)
+
         self.parent = parent
         self.item = item
         self.mainLayout = QtWidgets.QVBoxLayout(self)
@@ -16,7 +19,8 @@ class PriceHistoryGraph(QtWidgets.QDialog):
         self.graph.plotItem.vb.setMouseEnabled(x=False, y=False)
 
         self.graph.setLabel('left', "<span style=\"font-size:30px\">Price</span>")
-        self.graph.setLabel('bottom', "<span style=\"font-size:30px\">Days</span>")
+        self.graph.setLabel('bottom', "<span style=\"font-size:30px\">Day</span>")
+
         self.mainLayout.addWidget(self.graph)
 
         self.setWindowTitle("Price history for %s on %s" %
@@ -26,5 +30,17 @@ class PriceHistoryGraph(QtWidgets.QDialog):
         range_end = self.parent.state.day + 1
         range_start = (self.parent.state.day - len(self.item.value_history)) + 1
 
+        # Decrease font size of x-axis ticks
+        ax = self.graph.getAxis('bottom')
+        font = QtGui.QFont()
+        font.setPixelSize(12)
+        ax.setStyle(tickFont=font)
+
         x_axis = list(range(range_start, range_end))
         self.graph.plot(x_axis, self.item.value_history, pen='g')
+        self.graph.showGrid(x=True, y=True, alpha=0.5)
+        self.graph.setBackground('#101010')
+
+        # Ensure no fraction values (days) are shown on the X-axis
+        dx = [(value, str(value)) for value in list((range(int(min(x_axis)), int(max(x_axis)+1))))]
+        ax.setTicks([dx, []])
